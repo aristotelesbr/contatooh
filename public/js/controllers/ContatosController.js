577c1877d77d5948f7dc7a5e72e11f43aa8bbd4f
aristotelesbr <aristotelesbr2014@gmail.com>
2016-05-19 03:29:47 +0000

    Refatorando a exibição do total de contatos cadastrados

--- public/js/controllers/ContatosController.js
+++ public/js/controllers/ContatosController.js
@@ -1,31 +1,14 @@
-angular.module('contatooh').controller('ContatosController', function($scope){
-	$scope.total = 0;
-		$scope.incrementa = function() {
-				$scope.total++;
-	};
+angular.module('contatooh').controller('ContatosController', function($scope, $http){
+	$scope.contatos = [];
 
 	$scope.filtro = '';
 
-	$scope.contatos = [
-		{
-			"_id": 1,
-			"nome": "Ari",
-			"email": "ari@br.com"
-		},
-		{
-			"_id": 2,
-			"nome": "Lenna",
-			"email": "lenna@br.com"
-		},
-		{
-			"_id": 3,
-			"nome": "teste1",
-			"email": "teste1@br.com"
-		},
-		{
-			"_id": 4,
-			"nome": "Teste2",
-			"email": "teste2@br.com"
-		}
-	];
+	$http.get('/contatos')
+	.success(function(data) {
+		$scope.contatos = data;
+	})
+	.error(function(statusText) {
+		console.log("Não foi possivel obeter a lista de contatos");
+		console.log("statusText");
+	});
 });
