0bc5f284e26b6b8cf7ea75bb365944cb75915be4
aristotelesbr <aristotelesbr2014@gmail.com>
2016-05-22 13:43:23 +0000

    Exibindo aviso para usuario

--- public/js/controllers/ContatosController.js
+++ public/js/controllers/ContatosController.js
@@ -5,16 +5,20 @@ angular.module('contatooh').controller('ContatosController',
 
 		$scope.filtro = '';
 
+		$scope.mensagem = {text: ''};
+
 		var Contato =  $resource('/contatos/:id');
 
 		function buscaContatos() {
+			// A função query monta uma requisição do tipo GET para o recurso '/contatos'.
 			Contato.query(
 				function(contatos) {
 					$scope.contatos = contatos;
+					$scope.mensagem = {};
 				},
 				function(erro) {
-					console.log("Não foi possivel obter a lista de contatos");
 					console.log(erro);
+					$scope.mensagem = {"Não foi possivel obter a lista de contatos"};
 				}
 			);
 		}
@@ -24,7 +28,7 @@ angular.module('contatooh').controller('ContatosController',
 			Contato.delete({id: contato._id},
 				buscaContatos,
 				function(erro) {
-					console.log("Não foi pssovel remover o contato.");
+					$scope.mensagem = {"Não foi pssovel remover o contato."};
 					console.log(erro);
 				}
 			);
