2d44b94bd0c9dbf518a96b3d494d60170e55a622
aristotelesbr <aristotelesbr2014@gmail.com>
2016-05-29 13:33:52 +0000

    Gerenciando conexões 7.1

---
+++ config/database.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose');
+
+module.exports = function (uri) {
+	mongoose.connect(uri);
+
+	//Disparando mensagem para os eventos.
+	mongoose.connection.on('connected', function() {
+		console.log('Mongoose! Conectado em ' + uri);
+	});
+
+	mongoose.connection.on('disconnected', function() {
+		console.log('Mongoose! Disconectado de ' + uri);
+	});
+
+	mongoose.connection.on('error', function() {
+		console.log('Mongoose! Erro na conexão: ' + uri);
+	});
+
+	//Disparando mensagem no encerramento do servidor
+
+	process.on('SIGINT', function() {
+		mongoose.connection.close(function() {
+			console.log('Mongoose! Desconectado pelo termino da aplicação');
+			//0 Indica indeica que a finalização aconteceu sem errros.
+			process.exit(0);
+		});
+	});
+}
